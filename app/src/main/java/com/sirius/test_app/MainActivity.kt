8a6de91cd0d6b8f3dd05b5f1fe49a549efbfbb97
MatveyZi <matveysizov84@gmail.com>
2022-07-13 20:11:21 +0300

    Верстка старницы

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.sirius.test_app
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sirius.test_app.databinding.ActivityMainBinding
+
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
